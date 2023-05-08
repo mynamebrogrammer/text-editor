298bf5a9240c2057bf9704253c33d68918fea8bc
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-08 13:43:19 -0700

    ask BCS code review

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -24,20 +24,35 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
+// AskBCS CODE
 registerRoute(
-  ({ request }) => request.destination === 'script' || request.destination === 'style',
-  new CacheFirst({
-    cacheName: 'asset-cache',
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
     ],
   })
 );
+
+
+
+
+
+// TODO: Implement asset caching
+// registerRoute(
+//   ({ request }) => request.destination === 'script' || request.destination === 'style',
+//   new CacheFirst({
+//     cacheName: 'asset-cache',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//       new ExpirationPlugin({
+//         maxAgeSeconds: 30 * 24 * 60 * 60,
+//       }),
+//     ],
+//   })
+// );
--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -17,22 +17,20 @@ export const putDb = async (content) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put(content);
-  await tx.done;
+  const result = store.put({id:1, value: content});
+  const request = await result;
+  console.log(request);
   console.log('putDb completed');
 }
-console.error('putDb not implemented');
-
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const content = await store.getAll();
-  await tx.done;
+  const content =  store.get(1);
+  const result = await content;
+  console.log('result: ', result);
   console.log('getDb completed');
-  return content;
+  return result ?.value;
 }
-console.error('getDb not implemented');
-
 initdb();
