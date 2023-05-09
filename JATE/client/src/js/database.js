d4ccfb230323bc1d07b9716f2dc56000a43ff76e
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-08 22:13:44 -0700

    changed structure of files for heroku

---
+++ JATE/client/src/js/database.js
@@ -0,0 +1,36 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const result = store.put({id:1, value: content});
+  const request = await result;
+  console.log(request);
+  console.log('putDb completed');
+}
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content =  store.get(1);
+  const result = await content;
+  console.log('result: ', result);
+  console.log('getDb completed');
+  return result ?.value;
+}
+initdb();
